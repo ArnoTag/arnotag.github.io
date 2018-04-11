9aa8bccf7d98507ed9d69c84a693676c7c64c7ec
ArnoTag <38277554+ArnoTag@users.noreply.github.com>
2018-04-11 09:29:25 +0100

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,45 +1,29 @@
 window.onload=function(){
-console.log("ready");
-    setTimeout(function(){ 
+    setTimeout(function() {
+        var orderbutton = document.getElementById("place_order");
+        
+        if (orderbutton) {
+            orderbutton.addEventListener("click", function() {
+                var pricespan = document.querySelector(".order-total .woocommerce-Price-amount").textContent;
+                pricespan = parseFloat(pricespan.replace('€', ''));
+                gtag('event', 'Submit', {
+                    'event_category': 'Order',
+                    'value': pricespan
+                });
+            });
+        }
 
-  var orderbutton = document.getElementById("place_order");
+    }, 5000);
 
+    setTimeout(function() {
+        var cartbutton = document.getElementsByClassName("single_add_to_cart_button ");
+        if (cartbutton.length > 0) {
+            cartbutton[0].addEventListener("click", function() {
+                gtag('event', 'Submit', {
+                    'event_category': 'AddToCart'
+                });
+            });
+        }
+    }, 2000);
 
-  if (orderbutton) {
-  orderbutton.addEventListener("click", function() {
-    console.log("order");
- /*   gtag('event', 'Submit', {
-        'event_category': 'Order',
-        'value': pricespan       
-      }); */
-      
-  });
-
-
-
-  }
-
-} , 5000); 
-
-
-
-setTimeout(function(){
-
-    var cartbutton = document.getElementsByClassName("single_add_to_cart_button ");  
-
-
-  if (cartbutton.length > 0) {
-    cartbutton[0].addEventListener("click", function() {
-        event.preventDefault();
-        console.log("cart");
-     /*   gtag('event', 'Submit', {
-            'event_category': 'AddToCart'
-          }); */
-    });
-    }
-
-} , 1000);
-  
 };
-
-
